b514d80c9a0c24f67ac4bdcc9cd566f86414c618
Mateo Sierra <m.sierra1037@hotmail.com>
2024-04-24 15:36:37 -0500

    feat: refactored category api

--- lib/fauxmazon/products.ex
+++ lib/fauxmazon/products.ex
@@ -1,16 +1,19 @@
 defmodule Fauxmazon.Products do
   use Ecto.Schema
   import Ecto.Query
-  alias Fauxmazon.{Products,Categories, ProductsCategories, Repo}
-  @derive {Jason.Encoder, only: [
-    :id,
-    :name,
-    :description,
-    :price,
-    :brand,
-    :stock,
-    :image_url,
-    :rating,]}
+  alias Fauxmazon.{Products, Categories, ProductsCategories, Repo}
+
+  @derive {Jason.Encoder,
+           only: [
+             :id,
+             :name,
+             :description,
+             :price,
+             :brand,
+             :stock,
+             :image_url,
+             :rating
+           ]}
   schema "products" do
     field :name, :string
     field :description, :string
@@ -31,26 +34,32 @@ defmodule Fauxmazon.Products do
   def by_id(id) do
     Repo.get(Products, id)
   end
+
   def by_name(name) do
-    query = from p in Products,
-    where: ilike(p.name,^"%#{name}%")
+    query =
+      from p in Products,
+        where: ilike(p.name, ^"%#{name}%")
+
     Repo.all(query)
   end
 
   def by_category(category) do
-    products = from p in Products,
-    join: pc in ProductsCategories,
-    on: pc.product_id == p.id,
-    join: c in Categories,
-    on: pc.category_id == c.id,
-    where: c.id == ^category,
-    select: p
+    products =
+      from p in Products,
+        join: pc in ProductsCategories,
+        on: pc.product_id == p.id,
+        join: c in Categories,
+        on: pc.category_id == c.id,
+        where: c.id == ^category,
+        select: p
+
     category = from(c in Categories, where: c.id == ^category)
 
-    %{
-      "category" => Repo.all(category),
-      "products" => Repo.all(products)
-    }
+    category
+    |> Repo.all
+    |> List.first
+    |> Map.from_struct
+    |> Map.delete(:__meta__)
+    |> Map.merge(%{products: Repo.all(products)})
   end
-
 end
